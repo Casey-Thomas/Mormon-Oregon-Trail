6fa2a3b1a2a4d03375abe53c1148f156d63c3405
Laura <Laura@Richard-laptop>
2017-06-02 00:36:17 -0500

    @Team - Completed all steps in L07 Team Assignment - Step 2 - pages 36-48

--- src/mormonoregontrail/view/MainMenuView.java
+++ src/mormonoregontrail/view/MainMenuView.java
@@ -5,14 +5,118 @@
  */
 package mormonoregontrail.view;
 
+import java.util.Scanner;
+import mormonoregontrail.MormonOregonTrail;
+import mormonoregontrail.control.GameControl;
+
 /**
  *
- * @author Laura
+ * @author Team
  */
 public class MainMenuView {
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+    private String menu;
+    private String promptMessage;
+    
+    /**
+     * MainMenuView function
+     */
+    public MainMenuView() {
+        
+        this.promptMessage = "\nPlease choose an option: ";
+            
+        this.menu = "\n"
+                 + "\n------------------------------------------------"
+                 + "\n| Main Menu                                    |"
+                 + "\n------------------------------------------------"
+                 + "\nN - Start new game"
+                 + "\nG - Get and start saved game"
+                 + "\nH - Get help on how to play the game"
+                 + "\nS - Save game"
+                 + "\nQ - Quit"
+                 + "\n------------------------------------------------";
     }
     
+    public void displayMainMenuView() {
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+            
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\n" + this.menu);
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; // end the loop
+        }
+        
+        return value; // return the value entered
+    }
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice) {
+            case "N": // create and start a new game
+                this.startNewGame();
+                break;
+            case "G": // get and start an existing game
+                this.startExistingGame();
+                break;
+            case "H": // display the help menu
+                this.displayHelpMenu();
+                break;
+            case "S": // save the current game
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+        // create a new game
+        GameControl.createNewGame(MormonOregonTrail.getPlayer());
+        
+        // display the game menu
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void startExistingGame() {
+        System.out.println("\n*** startExistingGame() function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("\n*** displayHelpMenu() function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("\n*** saveGame() function called ***");
+    }
+
 }
